d757f873e4b842ff810128130136fb755b467079
Lipraxde <B10430214@mail.ntust.edu.tw>
2017-08-30 20:14:20 +0800

    New makefile test.

--- hello_gtk/gtk_hello.c
+++ hello_gtk/gtk_hello.c
@@ -1,9 +1,13 @@
 #include <gtk/gtk.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "test.h"
 
+// test comment
 int main(int argc, char *argv[])
 {
     GtkWidget *window;
-
+ 
     gtk_init(&argc, &argv);
     window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
     gtk_window_set_title(GTK_WINDOW(window), "HELLO ~ OuO ~");
@@ -11,6 +15,6 @@ int main(int argc, char *argv[])
                      G_CALLBACK(gtk_main_quit), NULL);
     gtk_widget_show(window);
     gtk_main();
-
+    foo();
     return 0;
 }
